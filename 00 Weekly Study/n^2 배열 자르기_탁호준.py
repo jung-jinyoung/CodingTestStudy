5e62ac22406bfbcc091da649c0a6703f166ad2cd
Jinyoung_Jung <ya_1001@naver.com>
2024-06-30 11:29:47 +0900

    00 study dir clear

--- 00 Weekly Study/n^2 배열 자르기_탁호준.py
+++
@@ -1,7 +0,0 @@
-def solution(n, left, right):
-    answer = []
-    for i in range(left, right+1):
-        share = i//n #몫
-        remainder = i%n #나머지
-        answer.append(max(share, remainder)+1)
-    return answer
