77574f7712573c684a2e9d34a9a407c36737fb57
Thoughtful-Lover <isso8336@gmail.com>
2024-07-08 20:53:48 +0900

    240708_cg

--- Thoughtful-Lover/240708/도둑질_이찬규.py
+++ Thoughtful-Lover/240708/도둑질_이찬규.py
@@ -3,7 +3,26 @@
 # 조건 3. 이웃한 두 집을 고를 수는 없다.
 
 
+from collections import deque
+
 def solution(money):
+    money_idx = deque()
+    length = len(money)
+    start = 0
+    while True:
+        if start >= length:
+            break
+        if start == 0:
+            start += 1
+        elif money[start] >= money[start+1]:
+            money_idx.append(start)
+            start += 1
+        else:
+            money_idx.append(start+1)
+            start += 2
+        continue
     answer = 0
+    for idx in money_idx:
+        answer += money[idx]
     return answer
 
