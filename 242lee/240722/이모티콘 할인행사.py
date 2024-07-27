c26a26c548ba850dd685e3241edac1e895dc3373
242lee <iamurmaster_@naver.com>
2024-07-27 20:54:37 +0900

    이소희 ReadMe 수정

---
+++ 242lee/240722/이모티콘 할인행사.py
@@ -0,0 +1,8 @@
+def solution(users, emoticons):
+    answer = []
+    return answer
+
+users = [[40, 10000], [25, 10000]]
+emoticons = [7000, 9000]
+result = solution(users, emoticons)
+print(result)
