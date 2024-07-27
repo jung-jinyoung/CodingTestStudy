20d549150a05d3d855d16bcd5cffee5ae7bfd806
242lee <iamurmaster_@naver.com>
2024-07-27 20:59:22 +0900

    이모티콘 할인행사 삭제

--- 242lee/240722/이모티콘 할인행사.py
+++
@@ -1,8 +0,0 @@
-def solution(users, emoticons):
-    answer = []
-    return answer
-
-users = [[40, 10000], [25, 10000]]
-emoticons = [7000, 9000]
-result = solution(users, emoticons)
-print(result)
